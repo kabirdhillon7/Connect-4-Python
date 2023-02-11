21124dcb2714e06030eefca603f3d4f06a490622
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-10 16:06:11 -0800

    Player selection functionality

--- connect4.py
+++ connect4.py
@@ -4,6 +4,18 @@ def create_board():
     board = np.zeros((6,7))
     return board
 
+board = create_board()
+game_over = False
+turn = 0
 
-ourBoard = create_board()
-print(ourBoard)
+while not game_over:
+    # Ask for Player 1 Input
+    if turn == 0:
+        selection = int(input("Player 1, make your selection (0-6): "))
+    # Ask for Player 2 Input
+    else:
+        selection = int(input("Player 2, make your selection (0-6): "))
+
+    turn += 1
+    # Alternate between P1 and P2's turns
+    turn = turn % 2
