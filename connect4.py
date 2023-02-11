b6a2db59ded69919ccc82e40fac47c1113d3e89b
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-10 16:12:45 -0800

    Built is_valid_location which is checking if a location is open or not

--- connect4.py
+++ connect4.py
@@ -1,20 +1,33 @@
 import numpy as np
 
+# Initilizes board
 def create_board():
     board = np.zeros((6,7))
     return board
 
+def drop_piece():
+    pass
+
+# Check if the top spot in a column is open or not
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row():
+    pass
+
 board = create_board()
+print(board)
+
 game_over = False
 turn = 0
 
 while not game_over:
     # Ask for Player 1 Input
     if turn == 0:
-        selection = int(input("Player 1, make your selection (0-6): "))
+        col = int(input("Player 1, make your selection (0-6): "))
     # Ask for Player 2 Input
     else:
-        selection = int(input("Player 2, make your selection (0-6): "))
+        col = int(input("Player 2, make your selection (0-6): "))
 
     turn += 1
     # Alternate between P1 and P2's turns
