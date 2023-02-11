d784eedb524bcdf86805f2b045709332e390bd15
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-10 18:27:10 -0800

    Built functionality to check for player wins

--- connect4.py
+++ connect4.py
@@ -14,7 +14,7 @@ def drop_piece(board, row, col, piece):
 
 # Check if the top spot in a column is open or not
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT - 1][col] == 0
 
 # Finds the next open row available
 def get_next_open_row(board, col):
@@ -27,6 +27,32 @@ def print_board(board):
     # flips board over x-axis so it's correctly formatted
     print(np.flip(board, 0))
 
+# Checks for wins
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for r in range(ROW_COUNT):
+        for c in range(COLUMN_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # Check positive sloped diagonals
+    for r in range(ROW_COUNT-3):
+        for c in range(COLUMN_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    # Check negative sloped diagonals
+    for r in range(ROW_COUNT-3):
+        # diagonal negative sloped wins can't start til the 4th row
+        for c in range(3, COLUMN_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
 
 # Game Set Up
 board = create_board()
@@ -44,6 +70,11 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
 
+            if winning_move(board, 1):
+                print("Player 1 Wins!")
+                game_over = True
+                break;
+
     # Player 2's turn
     else:
         col = int(input("Player 2, make your selection (0-6): "))
@@ -51,6 +82,11 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                print("Player 2 Wins!")
+                game_over = True
+                break;
 
     print_board(board)
 
